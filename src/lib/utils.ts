dbe28d383a0761b3035586d32ac1923a4f44e82f
zakaria mokhtari <zakaria.mokhtari@ensia.edu.dz>
2025-07-03 20:17:54 +0100

    update

---
+++ src/lib/utils.ts
@@ -0,0 +1,33 @@
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export function formatDate(input: string | number | Date): string {
+  const date = new Date(input);
+  return date.toLocaleDateString('en-US', {
+    month: 'long',
+    day: 'numeric',
+    year: 'numeric',
+  });
+}
+
+export function absoluteUrl(path: string) {
+  return `${process.env.NEXT_PUBLIC_APP_URL || ''}${path}`;
+}
+
+export function formatBytes(
+  bytes: number,
+  decimals = 0,
+  sizeType: 'accurate' | 'normal' = 'normal'
+) {
+  const sizes = ['Bytes', 'KB', 'MB', 'GB'];
+  const accurateSizes = ['Bytes', 'KiB', 'MiB', 'GiB'];
+  if (bytes === 0) return '0 Byte';
+  const i = Math.floor(Math.log(bytes) / Math.log(1024));
+  return `${(bytes / Math.pow(1024, i)).toFixed(decimals)} ${
+    sizeType === 'accurate' ? accurateSizes[i] ?? 'Bytest' : sizes[i] ?? 'Bytes'
+  }`;
+}
